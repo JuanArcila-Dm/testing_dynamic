7f4332f51710326a5e517b4371e4e9423672cde1
juanbobadilla14 <juanbobadilla2700@gmail.com>
2025-03-13 09:45:29 -0500

    Update script.js

--- client/script.js
+++ client/script.js
@@ -1,7 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
+    // Contenedor principal para cargar templates
     const contenedor = document.querySelector("#pantalla-templates");
-    const history = []; // Array para almacenar el historial de templates
 
+    // Función para mostrar un template por su ID
     const mostrarTemplate = (templateId) => {
         contenedor.innerHTML = ""; // Limpia el contenido del contenedor
 
@@ -9,63 +10,80 @@ document.addEventListener("DOMContentLoaded", () => {
         if (template) {
             const contenidoClonado = template.content.cloneNode(true);
             contenedor.appendChild(contenidoClonado);
-            history.push(templateId); // Agrega el template al historial
-            controlarVisibilidadCarrito(templateId); // Controla la visibilidad del botón
         } else {
             console.error(`Template con ID "${templateId}" no encontrado.`);
         }
     };
 
-    const controlarVisibilidadCarrito = (templateId) => {
-        const carrito = document.getElementById("carrito");
-        if (templateId === "pantallaPrincipal" || templateId === "descripcionProducto") {
-            carrito.style.display = "block"; // Muestra el botón
-        } else {
-            carrito.style.display = "none"; // Oculta el botón
-        }
-    };
-
-    // Cargar el template inicial
+    // Cargar el template inicial (pantalla principal)
     mostrarTemplate("pantallaPrincipal");
 
+    // Delegación de eventos para manejar cambios de template
     document.addEventListener("click", (event) => {
+
         // INICIO DE SESION
         if (event.target.id === "bt-inSesion") {
             mostrarTemplate("inicioDeSesion");
-        } else if (event.target.id === "bt-registrarse1") {
+        } if (event.target.id === "bt-registrarse1") {
             mostrarTemplate("registrarse");
-        } else if (event.target.id === "bt-recuperarC") {
+        } if (event.target.id === "bt-recuperarC") {
             mostrarTemplate("recuperacionContaseña1");
         }
         // REGISTRARSE
-        else if (event.target.id === "bt-registrarse2") {
+        if (event.target.id === "bt-registrarse2") {
             mostrarTemplate("verificarCorreo1");
-        } else if (event.target.id === "bt-verificar") {
-            mostrarTemplate("verificarCorreo2");
-        } else if (event.target.id === "bt-iniciar2") {
+        } if (event.target.id === "bt-verificar") {
+            mostrarTemplate("verificarCorreo2"); 
+        } if (event.target.id === "bt-iniciar2") {
             mostrarTemplate("inicioDeSesion");
         }
         // RECUPERACION DE CONTRASEÑA
-        else if (event.target.id === "bt-recuperar1") {
+        if (event.target.id === "bt-recuperar1") {
             mostrarTemplate("recuperacionContaseña2");
-        } else if (event.target.id === "bt-recuperar2") {
+        } if (event.target.id === "bt-recuperar2") {
             mostrarTemplate("recuperacionContaseña3");
-        } else if (event.target.id === "bt-iniciar3") {
+        } if (event.target.id === "bt-iniciar3") {
             mostrarTemplate("inicioDeSesion");
         }
-        // CARRITO DE COMPRAS
-        else if (event.target.id === "bt-carrito") {
-            mostrarTemplate("carritoCompras")
+        // DESCRIPCION PRODUCTO
+        if (event.target.id === "bt-productos") {
+            mostrarTemplate("descripcionProducto");
         }
-        // BOTON REGRESAR
-        else if (event.target.closest(".btn-regresar")) {
-            history.pop(); // Elimina el último template del historial
-            const previousTemplate = history[history.length - 1];
-            if (previousTemplate) {
-                mostrarTemplate(previousTemplate);
-            } else {
-                contenedor.innerHTML = ""; // Limpia el contenedor si no hay historial
-            }
+    });
+});
+
+// MOSTRAR LISTA CATEGORIA ADMIN
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector(".menu-button").addEventListener("click", function () {
+        document.getElementById("menu").classList.toggle("show");
+    });
+
+    window.addEventListener("click", function (event) {
+        if (!event.target.closest(".menu-container")) {
+            document.getElementById("menu").classList.remove("show");
         }
+    });
+});
+
+// <!---------------------- BOTON DE SCROLL TOP ---------------------------------->
+
+document.addEventListener("DOMContentLoaded", function () {
+    const scrollTopBtn = document.getElementById("scrollTopBtn");
+
+    // Mostrar el botón cuando se hace scroll hacia abajo
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 300) {
+            scrollTopBtn.style.display = "block";
+        } else {
+            scrollTopBtn.style.display = "none";
+        }
+    });
+
+    // Hacer scroll hacia arriba cuando se hace clic
+    scrollTopBtn.addEventListener("click", function () {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        });
     });
 });
